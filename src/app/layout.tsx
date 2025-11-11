dfac2c9041a438440287e8e7b4b2d40753d73c7f
plsgivemeachane <onewnew553@gmail.com>
2025-11-12 05:21:33 +0700

    speed & analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import { AppHeader } from '@/components/layout/AppHeader'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +25,8 @@ export default function RootLayout({
           <AppHeader />
           {children}
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
